33f5ea6e8bc3a7d7baab60d3078a763c77f761bc
zerinoid <leo.zerino@gmail.com>
2024-09-30 20:59:32 -0300

    feat: tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Em manutenção</h1>
-      <div className="card">
-        <p>
-          Para contato favor enviar um
-          <a href="mailto:isearte79@gmail.com"> email</a>
-        </p>
-      </div>
+      <h1>Meu novo site está em construção</h1>
+      <h2>
+        Para contato favor enviar um
+        <a target="_blank" href="mailto:isearte79@gmail.com">
+          {" "}
+          email
+        </a>
+      </h2>
     </>
   );
 }
